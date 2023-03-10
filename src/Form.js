5a9bd786ce4beff300797e89a0348c5bd568d3b8
Lincoln Wasden <lincolndwasden@gmail.com>
2023-03-10 08:58:43 -0600

    second

---
+++ src/Form.js
@@ -0,0 +1,210 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Form.css";
+
+function Form() {
+  const [metrics, setMetrics] = useState([]);
+  const [apiToken, setApiToken] = useState("");
+  const [metricTitle, setMetricTitle] = useState("");
+  const [metricId, setMetricId] = useState("");
+  const [metricUrl, setMetricUrl] = useState("");
+  const [showMetricTitleInput, setShowMetricTitleInput] = useState(false);
+  const [selectedMetrics, setSelectedMetrics] = useState([]);
+  const [metricUrls, setMetricUrls] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [addRequiredFilters, setAddRequiredFilters] = useState(false);
+
+  useEffect(() => {
+    if (apiToken !== "") {
+      axios
+        .get(`http://localhost:3001/api/embed/list/metric?token=${apiToken}`)
+        .then((response) => {
+          setMetrics(response.data.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [apiToken]);
+
+  const handleApiTokenSubmit = (event) => {
+    event.preventDefault();
+    setApiToken(event.target.elements.apiToken.value);
+    setShowMetricTitleInput(true);
+  };
+
+  const handleMetricSelect = (event) => {
+    const metricId = parseInt(event.target.value);
+    setSelectedMetrics((prevSelectedMetrics) => {
+      if (prevSelectedMetrics.includes(metricId)) {
+        return prevSelectedMetrics.filter((id) => id !== metricId);
+      } else {
+        return [...prevSelectedMetrics, metricId];
+      }
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSelectedMetrics([]);
+    setMetricUrls([]);
+    selectedMetrics.forEach((metricId) => {
+      const config = addRequiredFilters ? { requiredFilters: true } : {};
+      axios
+        .post(
+          `http://localhost:3001/api/embed/url/metric/${metricId}?token=${apiToken}`,
+          { config }
+        )
+        .then((response) => {
+          console.log(response.data);
+          setMetricUrls((prevMetricUrls) => [
+            ...prevMetricUrls,
+            response.data.data,
+          ]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  };
+
+  const filteredMetrics = metrics.filter((metric) =>
+    metric.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleGetMetricIdClick = (event) => {
+    event.preventDefault();
+    const metric = metrics.find(
+      (m) => m.title.toLowerCase() === metricTitle.toLowerCase()
+    );
+    if (metric) {
+      setMetricId(metric.id);
+    } else {
+      setMetricId("");
+      console.error(`Metric "${metricTitle}" not found`);
+    }
+  };
+
+  const handleGetMetricUrlClick = (event) => {
+    event.preventDefault();
+    axios
+      .post(
+        `http://localhost:3001/api/embed/url/metric/${metricId}?token=${apiToken}`,
+        { config: {} }
+      )
+      .then((response) => {
+        console.log(response.data);
+        setMetricUrl(response.data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <div className="form-container">
+      <div className="form">
+        <h1 className="form-heading">Metrics</h1>
+        {!showMetricTitleInput && (
+          <form onSubmit={handleApiTokenSubmit}>
+            <label htmlFor="apiToken" className="form-label">
+              API Token:
+            </label>
+            <input type="text" name="apiToken" id="apiToken" className="form-input" />
+            <button type="submit" className="form-button">Submit</button>
+          </form>
+        )}
+        {showMetricTitleInput && (
+          <div className="form-metrics-container">
+            <div className="form-metrics-background" />
+            <h2 className="form-metrics-title">Select Metrics</h2>
+            <input
+              type="text"
+              placeholder="Search metrics by title"
+              value={searchQuery}
+              onChange={(event) => setSearchQuery(event.target.value)}
+              className="form-metrics-search"
+            />
+            <div className="form-metrics-list">
+              {filteredMetrics.map((metric) => (
+                <div key={metric.id} className="form-metrics-item">
+                  <label>
+                    <input
+                      type="checkbox"
+                      value={metric.id}
+                      checked={selectedMetrics.includes(metric.id)}
+                      onChange={handleMetricSelect}
+                    />
+                    <span className="form-metrics-text">{metric.title}</span>
+                  </label>
+                  {selectedMetrics.includes(metric.id) && (
+                    <span className="form-metrics-selected">
+                      Selected
+                      <span className="form-metrics-selected-icon">&#x2714;</span>
+                    </span>
+                  )}
+                </div>
+              ))}
+            </div>
+            {selectedMetrics.length > 0 && metricUrls.length === 0 && (
+              <form onSubmit={handleSubmit} className="form-url-container">
+                <p className="form-text">Selected Metrics:</p>
+                <div className="form-checkbox-container">
+                  <label htmlFor="addFiltersCheckbox" className="form-checkbox-label">
+                    Add required filters:
+                    <input
+                      type="checkbox"
+                      id="addFiltersCheckbox"
+                      checked={addRequiredFilters}
+                      onChange={(event) =>
+                        setAddRequiredFilters(event.target.checked)
+                      }
+                      className="form-checkbox"
+                    />
+                  </label>
+                </div>
+                <div className="form-checkbox-list">
+                  {selectedMetrics.map((metricId) => {
+                    const metric = metrics.find((m) => m.id === metricId);
+                    return (
+                      <div key={metric.id}>
+                        <label>
+                          <input type="checkbox" checked disabled />
+                          <span className="form-checkbox-title">{metric.title}</span>
+                        </label>
+                      </div>
+                    );
+                  })}
+                </div>
+                <button
+                  type="submit"
+                  disabled={selectedMetrics.length === 0}
+                  className="form-button"
+                >
+                  Get Metric URLs
+                </button>
+              </form>
+            )}
+            {metricUrls.length > 0 && (
+              <div className="form-url-list">
+                <h2 className="form-metrics-title">Metric URLs:</h2>
+                <div>
+                  {metricUrls.map((url) => (
+                    <div key={url}>
+                      <a href={url} className="form-link">{url}</a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+  
+  
+  
+  
+}
+
+export default Form;
---
+++ src/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json()); // Parse JSON request body
+
+app.get('/api/embed/list/metric', async (req, res) => {
+  const token = req.query.token; // get the token from the query parameter
+  try {
+    const response = await axios.get('https://app.gogrow.com/api/embed/list/metric', {
+      headers: {
+        'Authorization': `Bearer ${token}` // use the token in the request header
+      }
+    });
+    const data = response.data;
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error');
+  }
+});
+
+app.post('/api/embed/url/metric/:id', async (req, res) => {
+  const { config } = req.body;
+  const token = req.query.token;
+  const id = req.params.id;
+  try {
+    const response = await axios.post(`https://app.gogrow.com/api/embed/url/metric/${id}`, { config }, {
+      headers: {
+        'Authorization': `Bearer ${token}` // use the token in the request header
+      }
+    });
+    const data = response.data;
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+module.exports = app;
